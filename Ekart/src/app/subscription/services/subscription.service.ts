d17cd0219e1189e75c8a2a6b6a553c141fd77422
SuganesanD <suganesan1808@gmail.com>
2024-10-10 18:10:19 +0530

    updated

--- Ekart/src/app/subscription/services/subscription.service.ts
+++ Ekart/src/app/subscription/services/subscription.service.ts
@@ -1,6 +1,5 @@
 export class subscriptionClass{
     subscriptionService(a:string){
-        alert(`thankyou for your `+a+` subscription!Enjoy your Subscription`)
+        alert(`Thankyou for your `+a+` subscription 🤗!Enjoy your Subscription`)
     }
-    
 }
