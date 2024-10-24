9df637ae72aa67b943a8b885314b121b71d6ff2c
SuganesanD <suganesan1808@gmail.com>
2024-10-24 15:01:30 +0530

    added forms component  and designed it to function it dynamically

--- Ekart/src/app/forms/forms.component.ts
+++ Ekart/src/app/forms/forms.component.ts
@@ -7,11 +7,34 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class FormsComponent {
 
-    Username:string=""
+  Username:string=""
+  a=0;
+  clickstate=0
+  clicklist:number[]=[]
+  paragrapharray:string[]=[]
+  para:string=""
 
-
-  
   click(){
+    this.paragrapharray.push(this.para)
+    this.a=0
+    this.clickstate=0
+    this.para="";
+    this.clickstate++
+    this.clicklist.push(this.clickstate) 
+  }
+  clickalert(){
     alert("Submitted!!!👍👍👍")
+  }
+  clickcontainer(){
+    if(this.para==""){
+      alert("Please enter the feedback") 
+    }
+    else{
+    this.click()
+    // setTimeout(() => {
+    //   this.clickalert()
+    // },500);
+    }
+    
   }
 }
