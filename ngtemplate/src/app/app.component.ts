05a613bd80cbb38ef8bc12ddbc59a2cd91ce8ace
SuganesanD <suganesan1808@gmail.com>
2024-11-05 15:52:24 +0530

    added new project for ng template

---
+++ ngtemplate/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngtemplate';
+  state:boolean=true
+  onclick(){
+    this.state=!this.state
+  }
+}
